f7b530f8c74cab36b4be2fa08e508bbfc8e8a76a
AbbasKhanPathan <abbaskhan21204@gmail.com>
2024-09-09 19:18:12 +0530

    commit1 AK landing_page

--- landing_page_1/README.md
+++
@@ -1,3 +0,0 @@
-# landing_page_1
-
-A new Flutter project.
--- landing_page_1/android/app/src/main/kotlin/com/example/landing_page_1/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.landing_page_1
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
