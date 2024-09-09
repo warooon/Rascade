017ddf8017010a6c349f2e1fa6bf9e68ef447e90
AbbasKhanPathan <abbaskhan21204@gmail.com>
2024-09-09 19:14:11 +0530

    second commit AK

--- landing_page_1/lib/main.dart
+++ landing_page_1/lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Landing Page Started'),
+          child: Text('Landing Page Started now '),
         ),
       ),
     );
